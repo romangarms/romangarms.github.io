5a0a2ecaabf5556fe407fe33ecc33a14d54f6e61
Roman Garms <romangarms@gmail.com>
2025-12-06 23:48:05 -0800

    fix footer mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <div id="footer">
-      <div style={{ marginLeft: '2%', marginRight: '1%' }} id="footer-tiles">
+      <div id="footer-tiles">
         <h2>Social Links</h2>
 
         <img width="24px" className="footer-icon" src="/images/github.svg" alt="GitHub Icon" />
@@ -22,25 +22,6 @@ function Footer() {
         <a href="https://www.instagram.com/romanogarmez/" target="_blank" rel="noopener noreferrer" id="footer-links">
           Instagram
         </a>
-      </div>
-
-      <div style={{ marginLeft: '1%', marginRight: '2%' }} id="footer-tiles">
-        <h2>Interactive Projects</h2>
-
-        <a href="https://nowplaying.romangarms.com/" target="_blank" rel="noopener noreferrer" id="footer-links">
-          Now Playing
-        </a>
-        <br />
-
-        <a href="https://tracker.romangarms.com/" target="_blank" rel="noopener noreferrer" id="footer-links">
-          WhereHaveIBeen
-        </a>
-        <br />
-
-        <a href="/UltimateTicTacToe/index.html" target="_blank" rel="noopener noreferrer" id="footer-links">
-          UltimateTicTacToe
-        </a>
-        <br />
       </div>
     </div>
   );
